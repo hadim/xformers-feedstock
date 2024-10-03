f68fa7322e9fd5d6230ca9b6e7002a1f3447d326
Hadrien Mary <hadrien.mary@gmail.com>
2024-10-03 13:59:38 -0400

    testing custom ci-setup

--- .scripts/run_osx_build.sh
+++ .scripts/run_osx_build.sh
@@ -31,7 +31,7 @@ mamba update --update-specs --yes --quiet --channel conda-forge --strict-channel
     pip mamba rattler-build conda-forge-ci-setup=4 "conda-build>=24.1"
 
 conda run pip uninstall -y conda-forge-ci-setup
-conda run pip install https://github.com/hadim/conda-forge-ci-setup-feedstock/archive/refs/heads/disable_cache.zip
+conda run pip install https://github.com/hadim/conda-forge-ci-setup-feedstock/archive/refs/heads/disable_cache.zip#subdirectory=recipe
 
 
 echo -e "\n\nSetting up the condarc and mangling the compiler."
